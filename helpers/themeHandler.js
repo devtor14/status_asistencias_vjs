a8d589e3af8aa9365956d8caffe1864d25f54dca
devtor14 <devtor14@gmail.com>
2026-05-09 00:20:27 -0400

    improve theme handler

--- helpers/themeHandler.js
+++ helpers/themeHandler.js
@@ -1,5 +1,18 @@
-if (document.cookie) {
-  document.documentElement.className = document.cookie;
-} else {
-  document.cookie = 'light';
+const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+function handleThemeChange(event) {
+  if (document.cookie) {
+    document.documentElement.className = document.cookie;
+    return;
+  }
+
+  if (event.matches) {
+    document.documentElement.className = 'dark';
+  } else {
+    document.documentElement.className = 'light';
+  }
 }
+
+colorSchemeQuery.addEventListener('change', handleThemeChange);
+
+handleThemeChange(colorSchemeQuery);
