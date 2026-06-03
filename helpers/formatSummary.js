7199d7ff9ed833d82b812700c1737c0570a34b29
devtor14 <devtor14@gmail.com>
2026-06-03 07:59:31 -0400

    update identificator message

--- helpers/formatSummary.js
+++ helpers/formatSummary.js
@@ -55,7 +55,7 @@ ${teamsHtml}
 ▪️ _Asistencias Por Facturar:_ *${stages['Por facturar']?.amount || 0}*
 ▪️ _Clientes atendidos Hoy:_ *${stages['Por facturar'].content.length + stages.Hecho.content.length}*
 
-\`\`\`Data procesada por AsysCheck\`\`\``;
+\`\`\`Procesada por AsysCheck 🤖\`\`\``;
 
   return { summary: generalSummary, report };
 };
